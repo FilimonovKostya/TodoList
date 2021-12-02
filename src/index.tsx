339c1d5dee1709fefbbb09748214acea5e04d9f0
FilimonovKostya <konstantinfilimonou@gmail.com>
2021-12-02 21:15:10 +0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import {Provider} from "react-redux";
 import store from "./Store";
 import App from "./Components/App/index.props";
 
+//
 ReactDOM.render(
     <Provider store={store}>
         <App />
